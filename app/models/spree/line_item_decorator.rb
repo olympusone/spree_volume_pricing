0f0a0344b9e33833aef8c5cb6c303dd575746110
dimidev <dimidev@olympusone.com>
2022-06-01 12:45:27 +0300

    update model

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,12 +1,4 @@
 module Spree::LineItemDecorator
-  # pattern grabbed from: http://stackoverflow.com/questions/4470108/
-
-  # the idea here is compatibility with spree_sale_products
-  # trying to create a 'calculation stack' wherein the best valid price is
-  # chosen for the product. This is mainly for compatibility with spree_sale_products
-  #
-  # Assumption here is that the volume price currency is the same as the product currency
-  
   def self.prepended(base)
     old_copy_price = base.instance_method(:copy_price)
 
@@ -14,15 +6,13 @@ module Spree::LineItemDecorator
       old_copy_price.bind(self).call
       return unless variant
 
-      if changed? && changes.keys.include?('quantity')
-        vprice = variant.volume_price(quantity, order.user)
+      vprice = variant.volume_price(quantity, order.user)
 
-        if price.present? && vprice <= variant.price
-          self.price = vprice and return
-        end
+      if self.price? && vprice <= variant.price
+        self.price = vprice and return
       end
 
-      self.price = variant.price if price.nil?
+      self.price = variant.price if self.price.nil?
     end
   end
 end
